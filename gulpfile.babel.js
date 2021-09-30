3cd91ca24fed98d8f7cb3cafbb8b3c2d33d56888
heammo12 <heammo12@gmail.com>
2021-09-30 19:41:37 +0900

    gulp-image

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
 
 
 const routes ={
@@ -9,8 +10,12 @@ const routes ={
         watch:"src/**/*.pug",
         src: "src/*.pug",
         dest :"build" 
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img"
     }
-}
+};
 
 const pug =() =>
   gulp
@@ -23,14 +28,23 @@ const clean = () => del(["build"]);
 const webserver = () => 
     gulp.src("build").pipe(ws({livereload: true, open :true}));
 
+const img = () => 
+  gulp
+    .src(route.img.src)
+    .pipe(image())
+    .pipe(gulp.dest(routes.img.dest));
+
+
 const watch = () =>{
     gulp.watch(routes.pug.watch, pug);
+    // gulp.watch(routes.img.src, img);
 };
 
-const prepare = gulp.series([clean]);
+
+const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug]);
 
-const postDev =gulp.series([webserver, watch]);
+const live =gulp.series([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const dev = gulp.series([prepare, assets, live]);
